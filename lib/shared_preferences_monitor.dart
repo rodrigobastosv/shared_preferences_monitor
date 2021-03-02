31d6a89464cd368743c4d9182e62e1615ea589ed
rodrigobastosv <rodrigobastosv@gmail.com>
2021-03-02 09:51:30 -0300

    migrating to null safe

--- lib/shared_preferences_monitor.dart
+++ lib/shared_preferences_monitor.dart
@@ -5,18 +5,18 @@ import 'package:shared_preferences/shared_preferences.dart';
 import 'package:shared_preferences_monitor/widget/widget.dart';
 
 class SharedPreferencesMonitor {
-  static SharedPreferences sharedPreferences;
-  static GlobalKey<NavigatorState> navigatorKey;
+  static late SharedPreferences sharedPreferences;
+  static late GlobalKey<NavigatorState> navigatorKey;
 
   /// Initializes the instance of the [SharedPreferences]. Should be execute on
   /// the startup of the app, mainly on the main method.
   ///
   /// Can receive a [GlobalKey] right on the init to handle navigation
-  static Future<void> init({GlobalKey<NavigatorState> key}) async {
+  static Future<void> init({GlobalKey<NavigatorState>? key}) async {
     if (key != null) {
       navigatorKey = key;
     }
-
+    
     sharedPreferences = await SharedPreferences.getInstance();
   }
 
@@ -45,12 +45,7 @@ class SharedPreferencesMonitor {
   ///
   /// Be sure to set the [navigationKey] before calling this method.
   static void showPage() async {
-    if (navigatorKey == null) {
-      throw Exception(
-          'The key for navigation is null. Assign a value to it to navigate to page');
-    }
-
-    navigatorKey.currentState.push(
+    navigatorKey.currentState!.push(
       MaterialPageRoute(
         builder: (_) => SharedPreferencesWidget(
           readOnly: false,
